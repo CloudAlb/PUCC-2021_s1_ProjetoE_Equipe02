070ee167c0a322f06f16acddd394fbfd73aefe07
Matheus Albino <matheusalb3213@gmail.com>
2021-03-31 13:15:58 -0300

    Conflitos do primeiro merge resolvidos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Home', url: '/cadastro-usuario', icon: 'home' },
-    { title: 'Meu perfil', url: '/folder/Meu Perfil', icon: 'person' },
+    { title: 'Home', url: 'home', icon: 'home' },
+    { title: 'Seu perfil', url: 'seu-perfil', icon: 'person' },
+    { title: 'Cadastro', url: '/cadastro-usuario', icon: 'person' },
     { title: 'Torneios', url: '/folder/Torneios', icon: 'trophy' },
     { title: 'Amigos', url: '/folder/Amigos', icon: 'heart' },
     { title: 'Loja', url: '/folder/Loja', icon: 'bag' },
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -13,12 +13,10 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
-<<<<<<< HEAD:src/app/pages/home/home.module.ts
-    HomePageRoutingModule
-=======
-    FolderPageRoutingModule,
+    HomePageRoutingModule,
+    // TODO, conflito
+    // FolderPageRoutingModule,
     ReactiveFormsModule
->>>>>>> 3db37aeb68e8b962bc0aedc1e6bbea363da86490:src/app/folder/folder.module.ts
   ],
   declarations: [HomePage]
 })
