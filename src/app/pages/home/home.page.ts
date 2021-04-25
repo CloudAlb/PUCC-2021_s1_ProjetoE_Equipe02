b0c69b873bcfec65320cd11023b5e200a908906d
Hzin <hugocoelhof03@gmail.com>
2021-04-25 16:38:14 -0300

    Mudança da home page | Imagens para a loja e background de pubs | e outras pequenas mudaças

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IonInfiniteScroll } from '@ionic/angular';
+import { AlertController, IonInfiniteScroll } from '@ionic/angular';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
@@ -16,17 +16,102 @@ export class HomePage implements OnInit {
   private readonly offset: number = 9;
   private index: number = 0;
 
+  public nameCamp = "Campeonato teste"
+  public jogoCamp = "League of legends"
+
   private name = "";
   private username = "";
   private avatar_image = "";
 
   constructor(private activatedRoute: ActivatedRoute,
-    private localStorageService: LocalStorageService) {
+    private localStorageService: LocalStorageService,
+    public alertController: AlertController) {
     for (let i = 0; i < 100; i++) {
       this.items.push(`Item ${i + 1}`)
     }
     this.itemsPage = this.items.slice(this.index, this.offset + this.index);
     this.index += this.offset;
+  }
+
+  async presentAlertRadio() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Meus campeonatos',
+      inputs: [
+        {
+          name: 'radio1',
+          type: 'radio',
+          label: 'Camp 1',
+          value: 'value1',
+          handler: () => {
+            console.log('Radio 1 selected');
+          },
+          checked: true
+        },
+        {
+          name: 'radio2',
+          type: 'radio',
+          label: 'Camp 2',
+          value: 'value2',
+          handler: () => {
+            console.log('Radio 2 selected');
+          }
+        },
+        {
+          name: 'radio3',
+          type: 'radio',
+          label: 'Camp 3',
+          value: 'value3',
+          handler: () => {
+            console.log('Radio 3 selected');
+          }
+        },
+        {
+          name: 'radio4',
+          type: 'radio',
+          label: 'Camp 4',
+          value: 'value4',
+          handler: () => {
+            console.log('Radio 4 selected');
+          }
+        },
+        {
+          name: 'radio5',
+          type: 'radio',
+          label: 'Camp 5',
+          value: 'value5',
+          handler: () => {
+            console.log('Radio 5 selected');
+          }
+        },
+        {
+          name: 'radio6',
+          type: 'radio',
+          label: 'Camp 6',
+          value: 'value6',
+          handler: () => {
+            console.log('Radio 6 selected');
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
   ngOnInit() {
